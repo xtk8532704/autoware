3f0800427ab23fb9493f98d6905b8e10d50ee754
Oguz Ozturk <oguzkaganozt@gmail.com>
2024-06-03 16:16:02 +0300

    update registry
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

--- docker/openadkit/startup/start_perception.sh
+++ docker/openadkit/startup/start_perception.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
 source /autoware/install/setup.bash
-ros2 launch /autoware/launch/perception.launch.xml map_path:=/autoware_map/sample-map-rosbag vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit
+ros2 launch /autoware/launch/perception.launch.xml map_path:=/autoware_map vehicle_model:=pixkit sensor_model:=pixkit_sensor_kit
--- docker/openadkit/startup/start_planning_sim.sh
+++ docker/openadkit/startup/start_planning_sim.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /opt/ros/humble/setup.bash
 source /autoware/install/setup.bash
-ros2 launch /autoware/launch/planning.launch.xml map_path:=/autoware_map/sample-map-rosbag vehicle_model:=sample_vehicle sensor_model:=sample_sensor_kit use_sim_time:=true launch_vehicle_interface:=false system_run_mode:=logging_simulation launch_system_monitor:=false launch_sensing_driver:=false
+ros2 launch /autoware/launch/planning.launch.xml map_path:=/autoware_map vehicle_model:=pixkit sensor_model:=pixkit_sensor_kit use_sim_time:=true launch_vehicle_interface:=false system_run_mode:=logging_simulation launch_system_monitor:=false launch_sensing_driver:=false
