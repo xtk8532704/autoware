b3c4e85c023d66d397e27071cbf04b04664196c3
Oguz Ozturk <oguzkaganozt@gmail.com>
2024-08-08 05:03:27 +0300

    add vanilla modules
    Signed-off-by: Oguz Ozturk <oguzkaganozt@gmail.com>

---
+++ docker/modules/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+#  The  run.sh  script is a simple bash script that runs the  docker compose up --build  command. This command will build the Docker image and run the application. 
+
+docker compose up --build
